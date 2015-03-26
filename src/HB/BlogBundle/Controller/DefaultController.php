3e063a0b0cfeb3baac0fc76a80654dcd8001776e
mfrutos <marc_frutos@yahoo.fr>
2015-03-26 14:34:10 +0100

    Ajout de l'entité Article par Symfony

--- src/HB/BlogBundle/Controller/DefaultController.php
+++ src/HB/BlogBundle/Controller/DefaultController.php
@@ -9,10 +9,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class DefaultController extends Controller
 {
     /**
+     * @Route("/")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+        /**
      * @Route("/hello/{name}")
      * @Template()
      */
-    public function indexAction($name)
+    public function helloAction($name)
     {
         return array('name' => $name);
     }
